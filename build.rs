e8c66a177f0c0d8f9627fc33e5b01dce4c784cdc
Kypo <100861025+gtasnail@users.noreply.github.com>
2024-09-07 17:20:19 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("./src/logo.ico"); // This should be the path to your icon
+        res.set_icon("./src/logo.ico"); 
         res.compile().expect("Failed to compile resources.");
     }
 }
